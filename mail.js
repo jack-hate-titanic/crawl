40a7c27796473e5ad229b70db0990dca1a2548e3
悦者生存 <1002783067@qq.com>
2022-10-22 18:57:34 +0800

    fix: 设置邮件相关配置

--- mail.js
+++ mail.js
@@ -2,7 +2,7 @@
  * @Author: 悦者生存 1002783067@qq.com
  * @Date: 2022-10-17 21:56:10
  * @LastEditors: 悦者生存 1002783067@qq.com
- * @LastEditTime: 2022-10-22 18:51:21
+ * @LastEditTime: 2022-10-22 18:55:57
  * @FilePath: /crawl/mail.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,14 +13,14 @@ let transporter = nodemailer.createTransport({
   port: 465,
   secureConnection: true,
   auth: {
-    user: '1002783067@qq.com',
-    pass: config.mailPass
+    user: config.mail.email,
+    pass: config.mail.pass
   }
 });
 
 function sendMail(to, subject, html) {
   let mailOptions = {
-    from: '"王森" <1002783067@qq.com>',
+    from: config.mail.from,
     to,
     subject,
     html,
